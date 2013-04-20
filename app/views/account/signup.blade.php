272098cbdd5ede61c4727e2b45e3640e7bc431bc
Johan Svensson <johan@atomicplayboy.net>
2013-04-21 01:25:02 +0200

    Signup form

---
+++ app/views/account/signup.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.default')
+
+@section('content')
+
+  <h2>Registrera ny användare</h2>
+
+  {{ Form::open() }}
+
+  <div>
+    {{ Form::label('email', 'Email') }} <br>
+    {{ Form::email('email') }} {{ $errors->first('email') }}
+  </div>
+
+  <div>
+    {{ Form::label('first_name', 'Förnamn') }} <br>
+    {{ Form::text('first_name') }} {{ $errors->first('first_name') }}
+  </div>
+
+  <div>
+    {{ Form::label('last_name', 'Efternamn') }} <br>
+    {{ Form::text('last_name') }} {{ $errors->first('last_name') }}
+  </div>
+
+  <div>
+    {{ Form::label('password', 'Lösenord') }} <br>
+    {{ Form::password('password') }} {{ $errors->first('password') }}
+  </div>
+
+  <div>
+    {{ Form::label('password_confirmation', 'Upprepa lösenord') }} <br>
+    {{ Form::password('password_confirmation') }} {{ $errors->first('password_confirmation') }}
+  </div>
+
+  <div>
+    {{ Form::token() }}
+    {{ Form::submit('Registrera konto') }}
+  </div>
+
+@stop
