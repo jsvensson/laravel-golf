3c576404de58a7219ad13949e4358662c7767a20
Johan Svensson <johan@atomicplayboy.net>
2013-04-21 11:35:29 +0200

    Basic view loads

--- app/controllers/PlayerController.php
+++ app/controllers/PlayerController.php
@@ -13,6 +13,18 @@ class PlayerController extends BaseController
     return View::make('player.index');
   }
 
+  public function getHome()
+  {
+    $user = Auth::user();
+
+    return View::make('player.home');
+  }
+
+  public function getShow($player_id)
+  {
+    return View::make('player.show');
+  }
+
 }
 
 /* EOF */
