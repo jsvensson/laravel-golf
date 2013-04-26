143e2794f4c004c4566b6f5bee04c82988e7e412
Johan Svensson <johan@atomicplayboy.net>
2013-04-26 22:45:57 +0200

    Auth'd username in menu

--- app/views/partial/nav/auth.blade.php
+++ app/views/partial/nav/auth.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav pull-right">
   <li id="fat-menu" class="dropdown">
-    <button id="usermenu" role="button" class="btn dropdown-toggle" data-toggle="dropdown">Mitt konto <span class="caret"></span></button>
+    <button id="usermenu" role="button" class="btn dropdown-toggle" data-toggle="dropdown">{{ $user->initial_name }} <span class="caret"></span></button>
     <ul class="dropdown-menu" role="menu" aria-labelledby="usermenu">
       <li role="presentation"><a role="menuitem" tabindex="-1" href="{{ URL::to('account/settings') }}">Inställningar</a></li>
       <li role="presentation" class="divider"></li>
