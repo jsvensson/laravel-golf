97bb38a058f8e95f254bab393439155accdad553
Johan Svensson <johan@atomicplayboy.net>
2013-05-08 13:59:05 +0200

    Brand link to front page

--- app/views/partial/nav.blade.php
+++ app/views/partial/nav.blade.php
@@ -1,7 +1,7 @@
   <div id="navbar" class="navbar navbar-fixed-top">
     <div class="navbar-inner">
       <div class="container">
-        <a class="brand" href="#">Golf</a>
+        <a class="brand" href="{{ url('/') }}">Golf</a>
         <ul class="nav" role="navigation">
           <li class="dropdown">
             <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Tävlingar <b class="caret"></b></a>
