967940e0710ab270df9130c9c712c890b0779a7e
Johan Svensson <johan@atomicplayboy.net>
2013-05-20 14:06:53 +0200

    Contest#postCreate scaffold

--- app/controllers/ContestController.php
+++ app/controllers/ContestController.php
@@ -35,6 +35,40 @@ class ContestController extends BaseController
     }
   }
 
+  public function postCreate()
+  {
+    if(Sentry::check()) {
+      return View::make('contest.create')
+        ->with('user', $this->user);
+    }
+    else {
+      return Redirect::to('account/login')
+        ->with('flash_error', 'Inloggning krävs.');
+    }
+
+    $val = Validator::make(
+      Input::all(),
+      [
+        'name' => 'required',
+        'date_start' => 'required|date_format:Y-m-d|before:' . Input::get('date_end'),
+        'date_end' => 'required|date_format:Y-m-d|after:' . Input::get('date_start'),
+      ]
+    );
+
+    if ($val->fails()) {
+      return Redirect::to('contest/create')
+        ->withInput(Input::all())
+        ->withErrors($val);
+    }
+    else {
+      // Validation passed, create contest
+      $c = [
+        'owner_id' => $this->user->id,
+        'name'     => Input::get('name'),
+      ];
+    }
+  }
+
 }
 
 /* EOF */
