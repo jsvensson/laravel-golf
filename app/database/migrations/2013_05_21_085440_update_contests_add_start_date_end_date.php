b919fdb7458f53dcaca6fc52e65207e88d946069
Johan Svensson <johan@atomicplayboy.net>
2013-05-21 11:13:29 +0200

    Contest migration
    Add DATE fields for start_date and end_date

---
+++ app/database/migrations/2013_05_21_085440_update_contests_add_start_date_end_date.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateContestsAddStartDateEndDate extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('contests', function($table)
+    {
+      $table->date('start_date');
+      $table->date('end_date');
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    Schema::table('contests', function($table)
+    {
+      $table->dropColumn('start_date', 'end_date');
+    });
+	}
+
+}
