50d7b76e4984a0a0ddd73fbb0e408e70a6356b5c
Johan Svensson <johan@atomicplayboy.net>
2013-05-22 13:58:55 +0200

    Wrong redirect target

--- app/filters.php
+++ app/filters.php
@@ -35,7 +35,7 @@ App::after(function($request, $response)
 
 Route::filter('auth', function()
 {
-	if (!Sentry::check()) return Redirect::route('login');
+	if (!Sentry::check()) return Redirect::to('account/login');
 });
 
 
