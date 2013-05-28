fdb755aafdc575c337374be5550885f4ed880194
Johan Svensson <johan@atomicplayboy.net>
2013-05-28 11:18:21 +0200

    Add is_open to contest table

---
+++ app/database/migrations/2013_05_28_091216_update_contests_add_open.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateContestsAddOpen extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    Schema::table('contests', function($table)
+    {
+     $table->boolean('is_open')
+       ->default(true);
+    });
+  }
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    Schema::table('contests', function($table)
+    {
+      $table->dropColumn('is_open');
+    });
+	}
+
+}
