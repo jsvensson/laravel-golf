355691d0c47dab420bb117d64dadfa7815170dde
Johan Svensson <johan@atomicplayboy.net>
2013-06-12 13:28:03 +0200

    Superuser seed

--- app/database/seeds/SentryTestUserSeeder.php
+++ app/database/seeds/SentryTestUserSeeder.php
@@ -4,6 +4,19 @@ class SentryTestUserSeeder extends Seeder
 {
   public function run()
   {
+    // Create initial superuser
+    $superuser = [
+    'email'      => 'johan@atomicplayboy.net',
+    'first_name' => 'Johan',
+    'last_name'  => 'Svensson',
+    'password'   => 'changeme'
+    ];
+    $su = User::register($superuser, true);
+
+    // Set superuser permissions
+    $group = Sentry::getGroupProvider()->findByName('superuser');
+    $su->addGroup($group);
+
     // Test users
     $test_users = [
       [
