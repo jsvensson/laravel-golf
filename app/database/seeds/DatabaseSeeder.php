339f4fd45d0e7926fb94201dabeef9dd4818a0a0
Johan Svensson <johan@atomicplayboy.net>
2013-06-13 16:58:43 +0200

    Some comments

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		// $this->call('UserTableSeeder');
-    $this->call('SentryGroupSeeder');
-    $this->call('SentryTestUserSeeder');
+    $this->call('SentryGroupSeeder');       // Create default groups
     $this->call('SentrySuperUserSeeder');   // Create superuser account
+    $this->call('SentryTestUserSeeder');    // Create test user accounts
 	}
 
 }
