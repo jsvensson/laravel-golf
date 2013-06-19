847b05f0b3163769ae1409b69c910fe8c0655ffa
Johan Svensson <johan@atomicplayboy.net>
2013-06-19 12:36:05 +0200

    Basic contest view

--- app/views/contest/show.blade.php
+++ app/views/contest/show.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
 
+  <h2>{{ $contest->name }}</h2>
+
+  <p>Ägare: <a href="{{ route('player.show', $contest->owner->id) }}">{{ $contest->owner->full_name }}</a></p>
+
+  <p>Spelare: <a href="{{ route('contest.players.index', $contest->id) }}">{{ $contest->player_count }}</a></p>
+
 @stop
