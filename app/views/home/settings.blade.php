4987b573b166122262fe273b503cf9abdf25f8f1
Johan Svensson <johan@atomicplayboy.net>
2013-06-24 15:07:45 +0200

    User settings form
    Signed-off-by: Johan Svensson <johan@atomicplayboy.net>

--- app/views/home/settings.blade.php
+++ app/views/home/settings.blade.php
@@ -2,4 +2,39 @@
 
 @section('content')
 
+<h2>Inställningar</h2>
+
+<a href="{{ URL::to('home/set-password') }}">Ändra lösenord</a>
+<a href="{{ URL::to('home/set-email') }}">Ändra lösenord</a>
+
+
+<h3>Kontouppgifter</h3>
+
+{{ Form::open(['url' => 'home/settings', 'method' => 'post', 'class' => 'form-horizontal']) }}
+
+  <div class="control-group">
+    <label class="control-label" for="first_name">Förnamn</label>
+    <div class="controls">
+      <input type="text" id="first_name" name="first_name" placeholder="Förnamn" value="{{ $user->first_name }}">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="last_name">Efternamn</label>
+    <div class="controls">
+      <input type="text" id="last_name" name="last_name" placeholder="Förnamn" value="{{ $user->last_name }}">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="handicap">Handikapp</label>
+    <div class="controls">
+      <input class="input-small" type="number" min="-100" max="100" id="handicap" name="handicap" placeholder="Handikapp" value="{{ $user->profile->handicap }}">
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <button type="submit" class="btn">Spara inställningar</button>
+    </div>
+  </div>
+{{ Form::close() }}
+
 @stop
