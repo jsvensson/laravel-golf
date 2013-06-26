0f57c0b18a98e95699e8f4d7a0cc0e2b3a791d07
Johan Svensson <johan@atomicplayboy.net>
2013-06-26 12:04:39 +0200

    Password change form
    Signed-off-by: Johan Svensson <johan@atomicplayboy.net>

--- app/views/home/password.blade.php
+++ app/views/home/password.blade.php
@@ -4,4 +4,39 @@
 
 <h2>Ändra lösenord</h2>
 
+{{ Form::open(['url' => 'home/password', 'method' => 'post', 'class' => 'form-horizontal']) }}
+
+  <div class="control-group">
+    <label class="control-label" for="current_password">Nuvarande lösenord</label>
+    <div class="controls">
+      {{ Form::password('current_password') }}
+      <span class="text-error">{{ $errors->first('current_password') }}</span>
+    </div>
+  </div>
+
+  <div class="control-group">
+    <label class="control-label" for="new_password">Nytt lösenord</label>
+    <div class="controls">
+      {{ Form::password('new_password') }}
+      <span class="text-error">{{ $errors->first('new_password') }}</span>
+    </div>
+  </div>
+
+  <div class="control-group">
+    <label class="control-label" for="repeat_new_password">Upprepa nytt lösenord</label>
+    <div class="controls">
+      {{ Form::password('repeat_new_password') }}
+      <span class="text-error">{{ $errors->first('repeat_new_password') }}</span>
+    </div>
+  </div>
+
+  <div class="control-group">
+    <div class="controls">
+      <button type="submit" class="btn">Ändra lösenord</button>
+    </div>
+  </div>
+
+
+{{ Form::close() }}
+
 @stop
