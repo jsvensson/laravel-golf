9f0cce457bb1b4af13d9428ddd4187d577705a2e
Johan Svensson <johan@atomicplayboy.net>
2013-07-17 14:08:10 +0200

    Add Pagoda production db

--- app/config/database.php
+++ app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'mysql_dev',
+	'default' => 'production',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -52,13 +52,24 @@ return array(
 			'prefix'   => '',
 		),
 
-		'mysql_dev' => array(
+		'development' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
 			'port'      => '8889',
 			'database'  => 'golf_l4',
 			'username'  => 'root',
 			'password'  => 'root',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+		'production' => array(
+			'driver'    => 'mysql',
+			'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+			'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'database',
+			'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'root',
+			'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
