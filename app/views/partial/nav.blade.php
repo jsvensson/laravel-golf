091b743cdc0350f020fa111e3388d955f82a8728
Johan Svensson <johan@atomicplayboy.net>
2013-07-19 17:53:18 +0200

    Currently unused menu items

--- app/views/partial/nav.blade.php
+++ app/views/partial/nav.blade.php
@@ -6,25 +6,11 @@
           <li class="dropdown">
             <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Tävlingar <b class="caret"></b></a>
             <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
-            @if(Sentry::check())
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="{{ url('home/contests') }}">Mina tävlingar</a></li>
-            @endif
               <li role="presentation"><a role="menuitem" tabindex="-1" href="{{ url('contest') }}">Öppna tävlingar</a></li>
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="{{ url('contest/finished') }}">Avslutade tävlingar</a></li>
             @if(Sentry::check())
               <li role="presentation" class="divider"></li>
               <li role="presentation"><a role="menuitem" tabindex="-1" href="{{ url('contest/create') }}">Skapa tävling</a></li>
             @endif
-            </ul>
-          </li>
-          <li class="dropdown">
-            <a href="#" id="drop2" role="button" class="dropdown-toggle" data-toggle="dropdown">Statistik <b class="caret"></b></a>
-            <ul class="dropdown-menu" role="menu" aria-labelledby="drop2">
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-              <li role="presentation" class="divider"></li>
-              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
             </ul>
           </li>
         </ul>
