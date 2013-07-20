a529284935107e90aa51f8e4e4543450788aaa11
Johan Svensson <johan@atomicplayboy.net>
2013-07-20 17:02:39 +0200

    ContestEvent scaffold

--- app/controllers/ContestEventController.php
+++ app/controllers/ContestEventController.php
@@ -5,10 +5,20 @@ class ContestEventController extends BaseController
 
   public function index($contest_id)
   {
+    return "Showing events for contest $contest_id";
+  }
 
+  public function show($contest_id, $event_id)
+  {
+    $events = Contest::find($contest_id)->events();
+    return "Showing event $event_id for contest $contest_id";
+  }
+
+  public function create($contest_id)
+  {
+    return "Creating event for contest $contest_id";
   }
 
 }
-
 
 /* EOF */
