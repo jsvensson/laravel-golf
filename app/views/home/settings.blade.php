eb190a82afe6a3ff41b1cc4cedcb58be4446ebe5
Johan Svensson <johan@atomicplayboy.net>
2013-07-24 11:26:04 +0200

    Whitespace

--- app/views/home/settings.blade.php
+++ app/views/home/settings.blade.php
@@ -19,18 +19,21 @@
       <input type="text" id="first_name" name="first_name" placeholder="Förnamn" value="{{ $user->first_name }}">
     </div>
   </div>
+
   <div class="control-group">
     <label class="control-label" for="last_name">Efternamn</label>
     <div class="controls">
       <input type="text" id="last_name" name="last_name" placeholder="Förnamn" value="{{ $user->last_name }}">
     </div>
   </div>
+
   <div class="control-group">
     <label class="control-label" for="handicap">Handikapp</label>
     <div class="controls">
       <input class="input-small" type="number" min="-100" max="100" id="handicap" name="handicap" placeholder="Handikapp" value="{{ $user->profile->handicap }}">
     </div>
   </div>
+
   <div class="control-group">
     <label class="control-label" for="website">Hemsida</label>
     <div class="controls">
@@ -43,6 +46,7 @@
       <button type="submit" class="btn">Spara inställningar</button>
     </div>
   </div>
+
 {{ Form::close() }}
 
 @stop
