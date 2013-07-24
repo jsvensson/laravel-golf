56cf9c70432a45b493baaa9112a3513eb0453aa3
Johan Svensson <johan@atomicplayboy.net>
2013-07-24 14:46:52 +0200

    Breadcrumbs config

---
+++ app/breadcrumbs.php
@@ -0,0 +1,30 @@
+<?php
+
+// Base Home breadcrumb
+Breadcrumbs::register('home', function($b) {
+  $b->push('Hem', URL::to('/'));
+});
+
+// contest.index breadcrumb
+Breadcrumbs::register('contests', function($b) {
+  $b->parent('home');
+  $b->push('Tävlingar', route('contest.index'));
+});
+
+// contest.show breadcrumb
+Breadcrumbs::register('contest', function($b, $contest) {
+  $b->parent('contests');
+  $b->push($contest->name, route('contest.show', $contest->id));
+});
+
+// contest.event.index breadcrumb
+Breadcrumbs::register('events', function($b, $contest) {
+  $b->parent('contest', $contest);
+  $b->push('Events', route('contest.event.index', $contest->id));
+});
+
+// contest.event.show breadcrumb
+Breadcrumbs::register('event', function($b, $event) {
+  $b->parent('events', $event->contest);
+  $b->push($event->name, route('contest.event.show', [$event->contest->id, $event->id]));
+});
