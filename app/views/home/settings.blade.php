29710b3adcef7b9387d4a33edad5571bb3b5a79a
Johan Svensson <johan@atomicplayboy.net>
2013-07-25 18:53:50 +0200

    Former

--- app/views/home/settings.blade.php
+++ app/views/home/settings.blade.php
@@ -11,42 +11,15 @@
 
 <h3>Kontouppgifter</h3>
 
-{{ Form::open(['url' => 'home/settings', 'method' => 'post', 'class' => 'form-horizontal']) }}
+{{ Former::horizontal_open() }}
+{{ Former::populate($user) }}
 
-  <div class="control-group">
-    <label class="control-label" for="first_name">Förnamn</label>
-    <div class="controls">
-      <input type="text" id="first_name" name="first_name" placeholder="Förnamn" value="{{ $user->first_name }}">
-    </div>
-  </div>
+  {{ Former::text('id') }}
+  {{ Former::text('first_name')->label('Förnamn') }}
+  {{ Former::text('last_name')->label('Efternamn') }}
+  {{ Former::number('profile.handicap')->label('Handikapp') }}
+  {{ Former::url('profile.website')->label('Hemsida') }}
 
-  <div class="control-group">
-    <label class="control-label" for="last_name">Efternamn</label>
-    <div class="controls">
-      <input type="text" id="last_name" name="last_name" placeholder="Förnamn" value="{{ $user->last_name }}">
-    </div>
-  </div>
-
-  <div class="control-group">
-    <label class="control-label" for="handicap">Handikapp</label>
-    <div class="controls">
-      <input class="input-small" type="number" min="-100" max="100" id="handicap" name="handicap" placeholder="Handikapp" value="{{ $user->profile->handicap }}">
-    </div>
-  </div>
-
-  <div class="control-group">
-    <label class="control-label" for="website">Hemsida</label>
-    <div class="controls">
-      <input class="input" type="url" id="website" name="website" placeholder="http://" value="{{ $user->profile->website }}">
-    </div>
-  </div>
-
-  <div class="control-group">
-    <div class="controls">
-      <button type="submit" class="btn">Spara inställningar</button>
-    </div>
-  </div>
-
-{{ Form::close() }}
+{{ Former::close() }}
 
 @stop
