17efa56212c7300f8785012b3b86bbaa86d3bd2e
Johan Svensson <johan@atomicplayboy.net>
2013-07-29 14:06:36 +0200

    Keep user name in User table

--- app/models/User.php
+++ app/models/User.php
@@ -123,16 +123,6 @@ class User extends SentryUserModel implements UserInterface, RemindableInterface
 		return $this->email;
 	}
 
-  public function getFirstNameAttribute()
-  {
-    return $this->profile->first_name;
-  }
-
-  public function getLastNameAttribute()
-  {
-    return $this->profile->last_name;
-  }
-
   public function getFullNameAttribute()
   {
     $full_name = trim("{$this->first_name} {$this->last_name}");
