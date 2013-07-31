e8fccff6ed1a224e4dd0736f526c0d49a6bf17ff
Johan Svensson <johan@atomicplayboy.net>
2013-07-31 13:25:55 +0200

    Add contest.player scaffold

--- app/controllers/ContestPlayerController.php
+++ app/controllers/ContestPlayerController.php
@@ -15,6 +15,14 @@ class ContestPlayerController extends BaseController {
       ->with('contest', $contest);
 	}
 
+  public function create($contest_id)
+  {
+    $contest = Contest::findOrFail($contest_id);
+
+    return View::make('contest.players.create')
+      ->with('contest', $contest);
+  }
+
   public function show($contest_id, $player_id)
   {
     return "TODO: Show contest $contest_id results for player $player_id";
