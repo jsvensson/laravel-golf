35c40cec2f130857aa53da5278037397fae2f1bc
Johan Svensson <johan@atomicplayboy.net>
2013-08-07 14:22:10 +0200

    Add players to contest

--- app/controllers/ContestPlayerController.php
+++ app/controllers/ContestPlayerController.php
@@ -25,6 +25,29 @@ class ContestPlayerController extends BaseController {
       ->with('contest', $contest);
   }
 
+  public function store($contest_id)
+  {
+    $contest = Contest::findOrFail($contest_id);
+
+    // Get integers out of the input while screaming in horror at this piece of code
+    $keys = [];
+    foreach(array_keys(Input::all()) as $arr) {
+      if (is_int($arr)) { $keys[] = $arr; }
+    }
+
+    // Attach players to contest
+    foreach($keys as $player_id) {
+      $contest->players()->attach($player_id, ['is_active' => true]);
+
+      // Attach all contest events to player
+      foreach($contest->events as $event) {
+        $event->players()->attach($player_id);
+      }
+    }
+
+    return Redirect::route('contest.show', [$contest->id]);
+  }
+
   public function show($contest_id, $player_id)
   {
     return "TODO: Show contest $contest_id results for player $player_id";
