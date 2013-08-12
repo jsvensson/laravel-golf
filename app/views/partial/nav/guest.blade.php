c3ad4c5309681502022efc0391b154bd5e3b0072
Johan Svensson <johan@atomicplayboy.net>
2013-08-12 13:23:57 +0200

    Use Former
    Input fields in plain HTML

--- app/views/partial/nav/guest.blade.php
+++ app/views/partial/nav/guest.blade.php
@@ -1,7 +1,9 @@
-{{ Form::open(['url' => 'auth/login', 'method' => 'post', 'class' => 'navbar-form pull-right']) }}
+{{ Former::open()
+    ->action(URL::to('auth/login'))
+    ->class('navbar-form pull-right') }}
 
   <input class="span2" type="email" name="email" placeholder="Email">
   <input class="span2" type="password" name="password" placeholder="Lösenord">
   <button type="submit" class="btn">Logga in</button>
 
-{{ Form::close() }}
+{{ Former::close() }}
