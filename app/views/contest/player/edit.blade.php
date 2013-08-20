5110787d0253ebd7941bdc928f0ef4f5babd5127
Johan Svensson <johan@atomicplayboy.net>
2013-08-20 13:40:00 +0200

    Basic edit events view

---
+++ app/views/contest/player/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.default')
+
+@section('content')
+
+  {{ Breadcrumbs::render('contest.player.edit', $contest, $player) }}
+
+  {{ Former::open()
+      ->action(URL::route('contest.player.update', [$contest->id, $player->id]))
+      ->method('put') }}
+
+  @if($player->events()->count() > 0)
+    <table>
+      <tr>
+        @foreach($player->eventsForContest($contest->id)->get() as $event)
+          <th>{{ $event->name }}</th>
+        @endforeach
+      </tr>
+      <tr>
+    @foreach($player->eventsForContest($contest->id)->get() as $event)
+      <td><input type="number" name="events[{{ $event->id }}][score]" value="{{ $event->pivot->score }}"></td>
+    @endforeach
+      </tr>
+    </table>
+  @endif
+
+  {{ Former::submit()->value('Spara poäng') }}
+
+  {{ Former::close() }}
+
+@stop
