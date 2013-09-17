da7c9b4843c5705d139df622fb6d21c5ce2de46e
Johan Svensson <johan@atomicplayboy.net>
2013-09-17 12:45:19 +0200

    contest.edit view scaffold

---
+++ app/views/contest/edit.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.default')
+
+@section('content')
+
+  {{ Breadcrumbs::render('contest.edit', $contest) }}
+
+  <h2>Redigera tävling</h2>
+
+<pre>{{ $contest }}</pre>
+
+  {{ Former::horizontal_open()
+      ->action(URL::route('contest.update', $contest->id))
+      ->method('put') }}
+  {{ Former::populate($contest) }}
+  {{ Former::text('name')
+      ->label('Namn')
+      ->placeholder('Tävling') }}
+  {{ Former::date('start_date')
+      ->label('Startdatum') }}
+  {{ Former::date('end_date')
+      ->label('Slutdatum') }}
+  {{ Former::submit() }}
+  {{ Former::close() }}
+
+@stop
