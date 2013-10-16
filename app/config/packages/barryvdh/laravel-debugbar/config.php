1bd496e1ef030343e4e3ba5ae337ff0dfd388511
Johan Svensson <johan@atomicplayboy.net>
2013-10-16 15:28:22 +0200

    Enable env, disable mail

--- app/config/packages/barryvdh/laravel-debugbar/config.php
+++ app/config/packages/barryvdh/laravel-debugbar/config.php
@@ -55,12 +55,12 @@ return array(
        'db' => true,                // Show database (PDO) queries and bindings
        'views' => true,             // Views with their data
        'route' => true,             // Current route information
-       'laravel' => false,          // Laravel version and environment
+       'laravel' => true,           // Laravel version and environment
        'events' => false,           // All events fired
        'twig' => false,             // Twig, requires barryvdh/laravel-twigbridge
        'default_request'=> false,   // Regular or special Symfony request logger
        'symfony_request'=> true,    // Only one can be enabled..
-       'mail' => true,              // Catch mail messages
+       'mail' => false,             // Catch mail messages
        'mail_log' => false,         // Display full mail log in messages
 
    )
