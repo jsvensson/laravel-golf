d8e00a74dff4e2b2d695ee1aedc926b289866372
Johan Svensson <johan@atomicplayboy.net>
2013-10-21 13:41:12 +0200

    Clean out old Bootstrap template

--- app/views/partial/nav.blade.php
+++ app/views/partial/nav.blade.php
@@ -1,38 +1,3 @@
-{{--
-<div class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="{{ URL::to('home') }}">Golf</a>
-    </div>
-    <div class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Tävlingar <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="{{ URL::route('contest.index') }}">Öppna tävlingar</a></li>
-          @if(!empty($user))
-            <li class="divider"></li>
-            <li><a href="{{ URL::route('contest.create') }}">Skapa ny tävling</a></li>
-          @endif
-          </ul>
-        </li>
-      </ul>
-
-      @if( ! empty($user))
-        @include('partial.nav.auth')
-      @else
-        @include('partial.nav.guest')
-      @endif
-    </div><!--/.navbar-collapse -->
-  </div>
-</div>
---}}
-
 <nav class="navbar navbar-inverse navbar-default" role="navigation">
   <!-- Brand and toggle get grouped for better mobile display -->
   <div class="navbar-header">
