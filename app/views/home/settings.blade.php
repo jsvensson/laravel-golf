7a41ce32f35aab27726638da48caf4e4cb7d2d77
Johan Svensson <johan@atomicplayboy.net>
2013-10-23 11:04:02 +0200

    Bypass bug in Former
    Do it in raw HTML for now until that gets fixed in Anahkiasen/former#166.

--- app/views/home/settings.blade.php
+++ app/views/home/settings.blade.php
@@ -16,8 +16,25 @@
 
   {{ Former::text('first_name')->label('Förnamn') }}
   {{ Former::text('last_name')->label('Efternamn') }}
+
+{{-- FIXME: Bug in Former, do this in raw HTML for now. }}
   {{ Former::number('profile.handicap')->label('Handikapp') }}
   {{ Former::url('profile.website')->label('Hemsida') }}
+--}}
+
+  <div class="control-group">
+    <label for="profile.handicap" class="control-label">Handikapp</label>
+    <div class="controls">
+      <input id="profile.handicap" type="number" name="profile.handicap" value="{{ $user->profile->handicap }}">
+    </div>
+  </div>
+
+  <div class="control-group">
+    <label for="profile.website" class="control-label">Hemsida</label>
+    <div class="controls">
+      <input id="profile.website" type="url" name="profile.website" value="{{ $user->profile->website }}">
+    </div>
+  </div>
 
   {{ Former::primary_submit()->value('Spara inställningar') }}
 
