5626ca14b4d9c3689d364020a4b05bc5ab24dccb
Johan Svensson <johan@atomicplayboy.net>
2013-10-30 11:01:29 +0100

    Indentation

--- app/views/layout/default.blade.php
+++ app/views/layout/default.blade.php
@@ -5,8 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-@stylesheets('bootstrapper')
-@stylesheets('application')
+  @stylesheets('bootstrapper')
+  @stylesheets('application')
 
 </head>
 <body>
