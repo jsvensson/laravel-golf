d7b4f007cd7a8ef2b45b1ff2a7199c7ee07b1939
Johan Svensson <johan@atomicplayboy.net>
2014-02-05 11:46:40 +0100

    Test controller cruft

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -3,34 +3,6 @@
 class TestController extends BaseController
 {
 
-  public function getScssphp()
-  {
-    $path = public_path() . '/assets/stylesheets/scss';
-    $s = new scssc();
-    $s->setImportPaths($path);
-    $css = $s->compile('@import "screen.scss"');
-
-    return View::make('test.css')
-      ->with('css', $css);
-  }
-
-  public function getAssetic()
-  {
-    // Filter manager
-    $fm = new Assetic\FilterManager();
-    $fm->set('scssphp', new Assetic\Filter\ScssphpFilter());
-
-    $path = public_path() . '/assets/stylesheets/scss';
-    $factory = new Assetic\Factory\AssetFactory($path);
-    $factory->setFilterManager($fm);
-    $factory->setDebug(true);
-
-    $css = $factory->createAsset('*.scss', 'scssphp');
-
-    return View::make('test.css')
-      ->with('css', $css->dump());
-  }
-
 }
 
 /* EOF */
