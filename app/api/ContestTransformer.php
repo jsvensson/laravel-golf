072d1b393adf566ac81e60e694c55d89f04f0325
Johan Svensson <johan@atomicplayboy.net>
2014-03-15 11:53:49 +0100

    Contest transformer

---
+++ app/api/ContestTransformer.php
@@ -0,0 +1,32 @@
+<?php
+
+use League\Fractal;
+
+class ContestTransformer extends Fractal\TransformerAbstract
+{
+
+  public function transform(Contest $contest)
+  {
+    $ary = [
+      'id'          => (int) $contest->id,
+      'owner_id'    => (int) $contest->owner_id,
+      'name'        => $contest->name,
+      'is_public'   => (int) $contest->is_public,
+      'is_finished' => (int) $contest->is_open,
+      'start_date'  => $contest->start_date,
+      'end_date'    => $contest->end_date,
+      'players'     => $contest->players->toArray(),
+      'events'      => [],
+    ];
+
+    // Massage events into proper format
+    foreach($contest->events as $e) {
+
+    }
+
+    return $ary;
+  }
+
+}
+
+/* EOF */
