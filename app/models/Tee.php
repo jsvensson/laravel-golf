e387bb9be142f1d7f072927258c0c38750e2fab0
Johan Svensson <johan@atomicplayboy.net>
2014-08-05 10:48:56 +0200

    forContest scope

--- app/models/Tee.php
+++ app/models/Tee.php
@@ -78,6 +78,11 @@ class Tee extends Eloquent
     $this->players()->attach($user->id);
   }
 
+  public function scopeForContest($query, Contest $contest)
+  {
+    return $query->where('contest_id', $contest->id);
+  }
+
 }
 
 /* EOF */
